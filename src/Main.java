6964877037919eacb00041b2cf7e8bf706ca1e85
Ufyanich <tima.sul.2001@gmail.com>
2023-07-28 15:38:46 +0600

    New Main class New graph object Check of addEdge method

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(0,3);
+        graph.addEdge(1,2);
+        graph.addEdge(2,3);
+        graph.addEdge(1,3);
+    }
+}
