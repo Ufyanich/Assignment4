ba21c6a37da373ff3618c7e80b4a4960c586f6d3
Ufyanich <tima.sul.2001@gmail.com>
2023-07-28 15:30:17 +0600

    Method for Adding Vertex Method for Adding Edge

--- src/Graph.java
+++ src/Graph.java
@@ -7,4 +7,13 @@ public class Graph<Vertex> {
     public Graph(){
         list = new HashMap<Vertex, List<Vertex>>();
     }
+
+    public void addVertex(Vertex vertex){
+        list.put(vertex, new LinkedList<Vertex>());
+    }
+
+    public void addEdge(Vertex source, Vertex var){
+        list.get(source).add(var);
+        list.get(var).add(source);
+    }
 }
