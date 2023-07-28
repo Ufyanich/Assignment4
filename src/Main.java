bdbbdf0e5a1eda1d741bac2cb09c180d037a37fa
Ufyanich <tima.sul.2001@gmail.com>
2023-07-28 16:11:42 +0600

    Addition of comments

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
 
-        //System.out.println("");
+        System.out.println("Addition of Vertexes");
+        System.out.println();
 
         graph.addVertex(0);
         graph.addVertex(1);
         graph.addVertex(2);
         graph.addVertex(3);
+
+        System.out.println("Edge between Vertexes");
+        System.out.println();
 
         graph.addEdge(0,1);
         graph.addEdge(0,2);
@@ -18,7 +22,13 @@ public class Main {
         graph.addEdge(2,3);
         graph.addEdge(1,3);
 
+        System.out.println("Show of the Graph");
+        System.out.println();
+
         graph.printGraph();
+
+        System.out.println("Test of Remove method and check through Graph print");
+        System.out.println();
 
         graph.removeEdge(0,3);
 
