8dd3084bdf5de4d056db7bd4a751bb66fb83f0be
Ufyanich <tima.sul.2001@gmail.com>
2023-07-29 15:38:26 +0600

    hasEdge method

--- src/Direct_graph.java
+++ src/Direct_graph.java
@@ -11,9 +11,20 @@ public class Direct_graph<Vertex> {
             throw new IllegalArgumentException("Vertex " + index + " is out of range");
         }
     }
+
+    public boolean hasEdge(Vertex source, Vertex var){
+        validateVertex(source);
+        validateVertex(var);
+        List<Vertex> neighbors = list.get(source);
+        return neighbors != null && neighbors.contains(var);
+    }
     public void DirectGraph(Vertex source, Vertex var){
         validateVertex(source);
         validateVertex(var);
+
+        for (int i = 0; i < list.size(); i++){
+
+        }
 
         list.get(source).add(var);
     }
