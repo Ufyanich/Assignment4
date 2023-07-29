98058a543b577dd39f45ab3b09910f4cbf043101
Ufyanich <tima.sul.2001@gmail.com>
2023-07-29 15:28:05 +0600

    New method to check valid variabels

--- src/Direct_graph.java
+++ src/Direct_graph.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class Direct_graph<Vertex> {
     private Map<Vertex, List<Vertex>> list;
-
-
+    private void validateVertex(Vertex index){
+        if(!list.containsKey(index)){
+            throw new IllegalArgumentException("Vertex " + index + " is out of range");
+        }
+    }
     public void DirectGraph(Vertex source, Vertex var){
         validateVertex(source);
         validateVertex(var);
