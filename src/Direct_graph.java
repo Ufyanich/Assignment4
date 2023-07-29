cc4cbbc9ea9b77b78d7b5e11de07ea1f546d3611
Ufyanich <tima.sul.2001@gmail.com>
2023-07-29 15:30:59 +0600

    New method for HashMap

--- src/Direct_graph.java
+++ src/Direct_graph.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class Direct_graph<Vertex> {
+
+    public Direct_graph(){
+        list = new HashMap<Vertex, List<Vertex>>();
+    }
     private Map<Vertex, List<Vertex>> list;
     private void validateVertex(Vertex index){
         if(!list.containsKey(index)){
